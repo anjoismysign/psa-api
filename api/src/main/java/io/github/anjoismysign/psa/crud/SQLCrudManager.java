fa04f739faf531abb2aa82a906970ee0c3f141a2
anjoismysign <lubensaico@gmail.com>
2026-03-24 21:42:11 -0600

    Formats SQLCrudManager

--- api/src/main/java/io/github/anjoismysign/psa/crud/SQLCrudManager.java
+++ api/src/main/java/io/github/anjoismysign/psa/crud/SQLCrudManager.java
@@ -5,27 +5,27 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public interface SQLCrudManager<T extends Crudable> extends CrudManager<T> {
-  String getCrudableKeyTypeName();
-  
-  String getTableName();
-  
-  String getPrimaryKeyName();
-  
-  default String crudableKeyTypePrepareStatement() {
-    return getCrudableKeyTypeName() + "=?";
-  }
-  
-  int getPrimaryKeyLength();
-  
-  Connection getConnection();
-  
-  boolean exists(String paramString);
-  
-  default void forEachRecord(Consumer<T> consumer) {
-    forEachRecord((crudable, version) -> consumer.accept(crudable));
-  }
-  
-  void forEachRecord(BiConsumer<T, Integer> paramBiConsumer);
-  
-  void reload();
+    String getCrudableKeyTypeName();
+
+    String getTableName();
+
+    String getPrimaryKeyName();
+
+    default String crudableKeyTypePrepareStatement() {
+        return getCrudableKeyTypeName() + "=?";
+    }
+
+    int getPrimaryKeyLength();
+
+    Connection getConnection();
+
+    boolean exists(String paramString);
+
+    default void forEachRecord(Consumer<T> consumer) {
+        forEachRecord((crudable, version) -> consumer.accept(crudable));
+    }
+
+    void forEachRecord(BiConsumer<T, Integer> paramBiConsumer);
+
+    void reload();
 }
